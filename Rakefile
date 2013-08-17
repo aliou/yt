fc544fc562cd04212ba08b17a1d179a58438fe7f
Aliou Diallo <aliou.diallo@me.com>
2013-08-17 02:12:33 +0200

    Start implementing fetching rake task.

--- Rakefile
+++ Rakefile
@@ -1,4 +1,8 @@
 require "sinatra/activerecord/rake"
 require "./app"
 
+require_relative "./tasks/fetch_channels"
 
+desc "Fetch channels and send new videos."
+task :fetch_channels do
+end
---
+++ tasks/fetch_channels.rb
@@ -0,0 +1,22 @@
+require "feedzirra"
+require "feedbag"
+
+def fetch_channels
+end
+
+def get_new_videos
+  current_time = Time.now
+  videos = []
+  Channel.all.each do |c|
+    feed = Feedzirra::Feed.fetch_and_parse(c.feed)
+    if feed.last_modified > c.last_fetched
+      videos += feed.entries.select { |f| f.publish > c.last_fetched }
+    end
+    c.last_fetched = current_time
+    c.save!
+  end
+  videos
+end
+
+def send_mail
+end
